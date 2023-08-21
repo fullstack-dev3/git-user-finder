c091826675eba4ca189de711d253be716ffe9acc
full-stack-david <davidchan12323@gmail.com>
2023-08-20 22:21:27 -0500

    Init project

---
+++ assets/scripts.js
@@ -0,0 +1,10 @@
+const apiUrl = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getUser(username)
+{
+    
+}
