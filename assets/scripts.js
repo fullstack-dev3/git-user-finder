812796515362d44fc92df9a403fdc5c8c8c69b29
full-stack-david <davidchan12323@gmail.com>
2023-08-20 23:51:30 -0500

    Add more user contents

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,28 +9,74 @@ getUser('full-stack-david')
 async function getUser(username)
 {
   const response = await fetch(apiUrl + username);
-  const result = await response.json();
-  
-  createUserCard(result);
+
+  if (response.status === 200) {
+    const repos = await getRepos(username);
+
+    const result = await response.json();
+
+    createUserCard(result, repos);
+  }
 }
 
-function createUserCard(user)
+async function getRepos(username) {
+  const response = await fetch(apiUrl + username + "/repos");
+  const result = await response.json();
+
+  const repos = document.createElement("div");
+
+  result
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .slice(0, 10)
+    .forEach((repo) => {
+      const repoEl = document.createElement("a");
+      repoEl.classList.add("repo");
+      repoEl.href = repo.html_url;
+      repoEl.target = "_blank";
+      repoEl.innerText = repo.name;
+
+      repos.appendChild(repoEl);
+    });
+
+  return repos.outerHTML;
+}
+
+function createUserCard(user, repos)
 {
+  let email = '';
+  let company = '';
+  let bio = '';
+  if (user.email) {
+    email = '<p>Email: ' + user.email + '</p>';
+  }
+  if (user.company) {
+    company = '<p>Company: ' + user.company + '</p>';
+  }
+  if (user.bio) {
+    bio = '<p>Bio: ' + user.bio + '</p>';
+  }
+
   const cardHTML = 
     `<div class = "card">
-      <div>
+      <div class="user-left">
         <img class="avatar" src ="${user.avatar_url}" alt="${user.name}" />
-      </div>
-      <div class="user-info">
+        ` +
+        email +
+        company +
+      `</div>
+      <div class="user-right">
         <h2>${user.name}</h2>
-        <p>Bio: ${user.bio}</p>
+        ` +
+        bio +
+        `<p>Github: <a target="_blank" href="${user.html_url}">${user.html_url}</a></p>
         <ul class="info">
           <li>${user.followers}<strong>Followers</strong></li>
           <li>${user.following}<strong>Following</strong></li>
           <li>${user.public_repos}<strong>Repos</strong></li>
         </ul>
-        <div id="repos"></div>
-      </div>
+        ` +
+        repos +
+      `</div>
     </div>`;
 
   main.innerHTML = cardHTML;
